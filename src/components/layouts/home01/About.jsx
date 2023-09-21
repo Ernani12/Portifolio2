9293f53dedbe91f82313eeb1cfe09e20b79cfd35
Ernani12 <39600025+Ernani12@users.noreply.github.com>
2023-09-21 18:12:04 -0300

    about me layout

--- src/components/layouts/home01/About.jsx
+++ src/components/layouts/home01/About.jsx
@@ -45,19 +45,20 @@ class About extends Component {
         return (
             <div className="background-white">
                 <div className="d-lg-flex">
-                    <div className="col-left">
-                        <div className="featured-post animate-element wow delay5 fadeInUp" data-wow-delay="0.5s">
+                    <div className="col-left" style={{backgroundColor:'white'}}>
+                        <div className="featured-post animate-element wow delay5 fadeInUp" data-wow-delay="0.5s" >
                             <img src="images/section/03.png" alt="images"/>
                         </div>
                     </div>
-                    <div className="col-right">
-                        <div className="flat-spacer" data-desktop="105" data-mobile="50" data-smobile="50"></div>
-                        <div className="flat-title t1">
-                            <div className="animate-element wow delay5 fadeInDown" data-wow-delay="0.5s">
-                                <h4 className="sub-title mg-b22">About Me</h4>
-                                <h2 className="title-section mg-b26 color-d12">Hi, I am here to <span className="color-d4">help you.</span></h2>
-                               <span  style={{color:'white', fontFamily:'Poynter', textAlign:'justify', fontSize:'20px', fontWeight:'bold'}}> <p>
-                                    <b>I am a technician and bachelor in Cumputer Science, 
+                    <div className="col-right" style={{backgroundColor:'white', border:'0px'}} >
+                        <div className="flat-spacer" data-desktop="105" data-mobile="50" data-smobile="50"  style={{backgroundColor:'white'}}></div>
+                        <div className="flat-title t1"  style={{backgroundColor:'white'}}>
+                            <div className="animate-element wow delay5 fadeInDown" data-wow-delay="0.5s" style={{backgroundColor:'white'}}>
+                                <h4 className="sub-title mg-b22" style={{backgroundColor:'white'}}>About Me</h4>
+                                <h2 className="title-section mg-b26 color-d12" style={{backgroundColor:'white'}}>Hi, I am here to <span className="color-d4" style={{backgroundColor:'white'}} >help you.</span></h2>
+                               <span   style={{fontFamily:"Times New Roman",color:'black', backgroundColor:'white', textAlign:'justify', fontSize:'18px', fontWeight:'bold'}}> 
+                               <p style={{backgroundColor:'white'}}>
+                                    I am a technician and bachelor in Cumputer Science, 
                                     starting my study in Java, POO, intarfaces, Rest, API,
                                     JSP, JFS, css, html since 2013. Java is scalable, 
                                     organized and can be adapted because to the JVM.
@@ -69,11 +70,11 @@ class About extends Component {
                                     <br></br>
                                     
                                     Hi  if you need Java softwares backend and frontend, maybe  fullstack 
-                                    web Rest API, automation, i could help  you.</b>
+                                    web Rest API, automation, i could help  you.
                                 </p> </span>
                             </div>
                             <div className="animate-element wow delay5 fadeInUp" data-wow-delay="0.5s">
-                                <div className="fl-btn btn-general">
+                                <div className="fl-btn btn-general" style={{backgroundColor:'white'}}>
                                     <Link to="#" className="f-w500 color-1 lt-sp07">Hire Me</Link>
                                 </div>
                             </div>
